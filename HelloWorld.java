1bc01fcb3208efe64ebbda979d16579af86672ef
The_Pi$ith <pisith@Pisiths-MacBook-Pro.local>
2024-04-01 15:09:15 +0700

    Implemented HelloWorld.java class

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I'm here again with Java");
+        System.out.println("We will go very deep and serious this time");
+    }
+
+}
